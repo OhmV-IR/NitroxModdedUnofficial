827faca31f597433fc97b41efdab49a275c968ce
Tornac <tornac@nitrox.rux.gg>
2024-04-16 08:50:37 -0400

    Add functions to register custom poster prefab(CREDIT TO TORNAC, this is his code) --co-authored-by="@Tornac"

--- NitroxClient/GameLogic/Spawning/WorldEntities/DefaultWorldEntitySpawner.cs
+++ NitroxClient/GameLogic/Spawning/WorldEntities/DefaultWorldEntitySpawner.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using NitroxModel.DataStructures;
@@ -193,6 +194,38 @@ public class DefaultWorldEntitySpawner : IWorldEntitySpawner, IWorldEntitySyncSp
 
         return false;
     }
+    static DefaultWorldEntitySpawner()
+    {
+        BuildPosterPrefab();
+    }
 
-    public bool SpawnsOwnChildren() => false;
+    private static void BuildPosterPrefab()
+    {
+        // REMOVE THIS COMMENT: Do your stuff creating the poster here, you can also import the prefab thanks from an asset bundle
+        GameObject prefab = new();
+
+        // REMOVE THIS CONTENT: Make sure to use the same TechType here as in BatchSpawner(or smth).cs
+        RegisterCustomPrefab((TechType)7575, prefab);
+    }
+
+    public static void RegisterCustomPrefab(TechType techType, GameObject customPrefab)
+    {
+        if (prefabCacheByTechType.ContainsKey(techType))
+        {
+            throw new ArgumentException($"Can't register custom prefab {customPrefab.name} because another one is already registered with TechType {techType}.");
+        }
+        prefabCacheByTechType.Add(techType, customPrefab);
+        Log.Info($"Successfully register custom prefab {customPrefab.name} with TechType: {techType}");
+    }
+
+    public static void RegisterCustomPrefab(string classId, GameObject customPrefab)
+    {
+        if (prefabCacheByClassId.ContainsKey(classId))
+        {
+            throw new ArgumentException($"Can't register custom prefab {customPrefab.name} because another one is already registered with class id {classId}.");
+        }
+        prefabCacheByClassId.Add(classId, customPrefab);
+        Log.Info($"Successfully register custom prefab {customPrefab.name} with class id: {classId}");
+    }
+public bool SpawnsOwnChildren() => false;
 }
